4f791508d7758182fd1fbd667321d66c97b4deae
Jake McKenzie <jake314@uw.edu>
2025-04-10 16:03:42 -0600

    longer input

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 )
 
 func main() {
-	input := "let x = 5;"
+	input := `let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+		x + y;
+	}
+	fn(ten, ten)`
 	l := lexer.New(input)
 	for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 		fmt.Printf("%+v\n", tok)

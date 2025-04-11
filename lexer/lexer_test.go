8f5ddf410b1b7a9afab26df5a9d8bb1a7fe2aa63
Jake McKenzie <jake314@uw.edu>
2025-04-10 22:52:45 -0600

    refactored unit test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -13,15 +13,10 @@ let fn(ten, five);
 let add = fn(x, y) {
 	x + y;
 };
-let result = add(five, ten);
-!-/*5;
-5 < 10 > 5;
-if (5 < 10) {
-	return true;
-} else {
-	return false;
-};
-
+x == y;
+bob != alice;
+return true;
+let money = false;
 `
 
 	expected := []token.Token{
@@ -61,82 +56,36 @@ if (5 < 10) {
 		{Type: token.RPAREN, Literal: ")"},
 		{Type: token.LBRACE, Literal: "{"},
 
-		// // x + y;
+		// x + y;
 		{Type: token.IDENT, Literal: "x"},
 		{Type: token.PLUS, Literal: "+"},
 		{Type: token.IDENT, Literal: "y"},
 		{Type: token.SEMICOLON, Literal: ";"},
 
-		// // };
+		// };
 		{Type: token.RBRACE, Literal: "}"},
 		{Type: token.SEMICOLON, Literal: ";"},
 
-		// let result = add(five, ten);
-		{Type: token.LET, Literal: "let"},
-		{Type: token.IDENT, Literal: "result"},
-		{Type: token.ASSIGN, Literal: "="},
-		{Type: token.IDENT, Literal: "add"},
-		{Type: token.LPAREN, Literal: "("},
-		{Type: token.IDENT, Literal: "five"},
-		{Type: token.COMMA, Literal: ","},
-		{Type: token.IDENT, Literal: "ten"},
-		{Type: token.RPAREN, Literal: ")"},
+		// x == y;
+		{Type: token.IDENT, Literal: "x"},
+		{Type: token.EQ, Literal: "=="},
+		{Type: token.IDENT, Literal: "y"},
 		{Type: token.SEMICOLON, Literal: ";"},
-
-		// !-/*5;
-		{Type: token.BANG, Literal: "!"},
-		{Type: token.MINUS, Literal: "-"},
-		{Type: token.SLASH, Literal: "/"},
-		{Type: token.ASTERISK, Literal: "*"},
-		{Type: token.INT, Literal: "5"},
+		// bob != alice;
+		{Type: token.IDENT, Literal: "bob"},
+		{Type: token.NOT_EQ, Literal: "!="},
+		{Type: token.IDENT, Literal: "alice"},
 		{Type: token.SEMICOLON, Literal: ";"},
-
-		// 5 < 10 > 5;
-		{Type: token.INT, Literal: "5"},
-		{Type: token.LT, Literal: "<"},
-		{Type: token.INT, Literal: "10"},
-		{Type: token.GT, Literal: ">"},
-		{Type: token.INT, Literal: "5"},
-		{Type: token.SEMICOLON, Literal: ";"},
-
-		// if (5 < 10) {
-		{Type: token.IF, Literal: "if"},
-		{Type: token.LPAREN, Literal: "("},
-		{Type: token.INT, Literal: "5"},
-		{Type: token.LT, Literal: "<"},
-		{Type: token.INT, Literal: "10"},
-		{Type: token.RPAREN, Literal: ")"},
-		{Type: token.LBRACE, Literal: "{"},
-
 		// return true;
 		{Type: token.RETURN, Literal: "return"},
 		{Type: token.TRUE, Literal: "true"},
 		{Type: token.SEMICOLON, Literal: ";"},
-
-		// } else {
-		{Type: token.RBRACE, Literal: "}"},
-		{Type: token.ELSE, Literal: "else"},
-		{Type: token.LBRACE, Literal: "{"},
-
-		// return false;
-		{Type: token.RETURN, Literal: "return"},
+		// let money = false;
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "money"},
+		{Type: token.ASSIGN, Literal: "="},
 		{Type: token.FALSE, Literal: "false"},
 		{Type: token.SEMICOLON, Literal: ";"},
-
-		// };
-		{Type: token.RBRACE, Literal: "}"},
-
-		// // 10 == 10;
-		// {Type: token.INT, Literal: "10"},
-		// {Type: token.EQ, Literal: "=="},
-		// {Type: token.INT, Literal: "10"},
-		// {Type: token.SEMICOLON, Literal: ";"},
-
-		// // 10 != 9;
-		// {Type: token.INT, Literal: "10"},
-		// {Type: token.NOT_EQ, Literal: "!="},
-		// {Type: token.INT, Literal: "9"},
-		// {Type: token.SEMICOLON, Literal: ";"},
 	}
 
 	l := New(input)
